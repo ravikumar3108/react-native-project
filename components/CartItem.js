6047724244271651f373168ee477141e05922afb
Ravi Kumar <raviluhaniwal318@gmail.com>
2025-10-14 15:06:14 +0530

    quantity

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,21 +1,52 @@
+import { useState } from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function CartItem({ item }) {
-  return (
 
+  const [quantity, setquantity] = useState(item?.quantity)
+  const [total, setTotal] = useState([])
+
+
+  function addQuantity() {
+    setquantity(quantity + 1)
+  }
+
+
+  function decQuantity() {
+    if (quantity <= 1) {
+      setquantity(1)
+    }
+    else {
+      setquantity(quantity - 1)
+    }
+  }
+
+  function TotalPrice() {
+    console.log(item?.item?.price)
+
+  }
+
+
+  return (
     <View style={styles.card}>
       <Image source={{ uri: item.image }} style={styles.image} />
       <View style={styles.info}>
         <Text style={styles.name}>{item?.item?.title}</Text>
-        <Text style={styles.price}>${item?.item?.price * item?.quantity}</Text>
+        <Text style={styles.price}>${item?.item?.price * quantity}</Text>
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.qtyButton}>
+          {quantity == 1 ? <TouchableOpacity style={styles.qtyButton} onPress={decQuantity} disabled>
             <Text style={styles.qtyText}>-</Text>
-          </TouchableOpacity>
-          <Text style={styles.qty}>{item?.quantity}</Text>
-          <TouchableOpacity style={styles.qtyButton}>
+          </TouchableOpacity> : <TouchableOpacity style={styles.qtyButton} onPress={decQuantity}>
+            <Text style={styles.qtyText}>-</Text>
+          </TouchableOpacity>}
+          <Text style={styles.qty}>{quantity}</Text>
+          <TouchableOpacity style={styles.qtyButton} onPress={addQuantity}>
             <Text style={styles.qtyText}>+</Text>
           </TouchableOpacity>
+          {/* <TouchableOpacity style={styles.qtyButton} onPress={TotalPrice(quantity)}>
+            <Text style={styles.qtyText}>Total</Text>
+          </TouchableOpacity>
+          <Text style={styles.qty}>Total :- {item?.item?.price * quantity}</Text> */}
         </View>
       </View>
       <TouchableOpacity style={styles.removeButton} >
